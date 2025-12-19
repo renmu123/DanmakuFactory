9dd33f480b69a7fe9109891aaaf26894f5f24ede
muse <fyx@030buy.jp>
2025-12-19 18:21:05 +0800

    Revert "style(format): AUTO format code"
    This reverts commit e75b2108f73955e7bc79ccac654010b31b417a01.

--- src/AssFile/AssFile.c
+++ src/AssFile/AssFile.c
@@ -22,8 +22,8 @@
  */
 
 #include "AssFile.h"
-#include "../Define/CLIDef.h"
 #include "../FileUtil/FileUtil.h"
+#include "../Define/CLIDef.h"
 #include "AssStringProcessing.h"
 
 static int printStatDataStr(FILE *filePtr, const int startTime, const int endTime, const int posX, const int posY,
