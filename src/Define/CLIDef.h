dd7f5cb1c8f830c31178b41f075aa845954ba705
muse <fyx@030buy.jp>
2026-04-24 12:01:22 +0800

    bump version 2.1.2

--- src/Define/CLIDef.h
+++ src/Define/CLIDef.h
@@ -26,7 +26,7 @@
 
 #define GUI_SUPPORT // 给GUI版本提供一些特殊的功能支持
 
-#define VERSION "2.1.1"
+#define VERSION "2.1.2"
 #define EDITION "CLI Release"
 #define CONFIG_FILE_NAME "DanmakuFactoryConfig.json"
 #define FILENAME_LEN 1024
